6a539ad3c24b29613d5cc1d391b3fdc1a2242f4e
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-02-24 04:36:32 -0700

    More cleaning up.

--- test/sme_clj/core_test.clj
+++ test/sme_clj/core_test.clj
@@ -131,41 +131,42 @@
                                  [:pressure-Vial :temperature-Icecube]
                                  [:Vial :Icecube]})
 
-(def expected-hypothesis-structure {[:Beaker :Coffee]                      {:emaps  #{[:Beaker :Coffee]}
-                                                                            :nogood #{[:Water :Coffee]}}
-                                    [:Vial :Icecube]                       {:emaps #{[:Vial :Icecube]}}
-                                    [:Water :Heat]                         {:emaps  #{[:Water :Heat]}
-                                                                            :nogood #{[:Water :Coffee]}}
-                                    [:Pipe :Bar]                           {:emaps #{[:Pipe :Bar]}}
-                                    [:Water :Coffee]                       {:emaps  #{[:Water :Coffee]}
-                                                                            :nogood #{[:Water :Heat]
-                                                                                      [:Beaker :Coffee]}}
-                                    [:flat-top-Water :flat-top-Coffee]     {:children #{[:Water :Coffee]}}
-                                    [:liquid-Water :liquid-Coffee]         {:children #{[:Water :Coffee]}}
-                                    [:pressure-Vial :temperature-Icecube]  {:nogood   #{[:diameter-Vial :temperature-Icecube]}
-                                                                            :children #{[:Vial :Icecube]}}
-                                    [:pressure-Beaker :temperature-Coffee] {:nogood   #{[:diameter-Beaker :temperature-Coffee]}
-                                                                            :children #{[:Beaker :Coffee]}}
-                                    [:diameter-Beaker :temperature-Coffee] {:nogood   #{[:pressure-Beaker :temperature-Coffee]}
-                                                                            :children #{[:Beaker :Coffee]}}
-                                    [:diameter-Vial :temperature-Icecube]  {:nogood   #{[:pressure-Vial :temperature-Icecube]}
-                                                                            :children #{[:Vial :Icecube]}}
-                                    [:greater-diameter-Beaker-diameter-Vial
-                                     :greater-temperature-Coffee-temperature-Icecube]
-                                    {:nogood   #{[:greater-pressure-Beaker-pressure-Vial
-                                                  :greater-temperature-Coffee-temperature-Icecube]}
-                                     :children #{[:diameter-Beaker :temperature-Coffee]
-                                                 [:diameter-Vial :temperature-Icecube]}}
-                                    [:flow-Beaker-Vial-Water-Pipe :flow-Coffee-Icecube-Heat-Bar]
-                                    {:children #{[:Beaker :Coffee]
-                                                 [:Vial :Icecube]
-                                                 [:Pipe :Bar]
-                                                 [:Water :Heat]}}
-                                    [:greater-pressure-Beaker-pressure-Vial :greater-temperature-Coffee-temperature-Icecube]
-                                    {:nogood   #{[:greater-diameter-Beaker-diameter-Vial
-                                                  :greater-temperature-Coffee-temperature-Icecube]}
-                                     :children #{[:pressure-Vial :temperature-Icecube]
-                                                 [:pressure-Beaker :temperature-Coffee]}}})
+(def expected-hypothesis-structure
+  {[:Beaker :Coffee]                      {:emaps  #{[:Beaker :Coffee]}
+                                           :nogood #{[:Water :Coffee]}}
+   [:Vial :Icecube]                       {:emaps #{[:Vial :Icecube]}}
+   [:Water :Heat]                         {:emaps  #{[:Water :Heat]}
+                                           :nogood #{[:Water :Coffee]}}
+   [:Pipe :Bar]                           {:emaps #{[:Pipe :Bar]}}
+   [:Water :Coffee]                       {:emaps  #{[:Water :Coffee]}
+                                           :nogood #{[:Water :Heat]
+                                                     [:Beaker :Coffee]}}
+   [:flat-top-Water :flat-top-Coffee]     {:children #{[:Water :Coffee]}}
+   [:liquid-Water :liquid-Coffee]         {:children #{[:Water :Coffee]}}
+   [:pressure-Vial :temperature-Icecube]  {:nogood   #{[:diameter-Vial :temperature-Icecube]}
+                                           :children #{[:Vial :Icecube]}}
+   [:pressure-Beaker :temperature-Coffee] {:nogood   #{[:diameter-Beaker :temperature-Coffee]}
+                                           :children #{[:Beaker :Coffee]}}
+   [:diameter-Beaker :temperature-Coffee] {:nogood   #{[:pressure-Beaker :temperature-Coffee]}
+                                           :children #{[:Beaker :Coffee]}}
+   [:diameter-Vial :temperature-Icecube]  {:nogood   #{[:pressure-Vial :temperature-Icecube]}
+                                           :children #{[:Vial :Icecube]}}
+   [:greater-diameter-Beaker-diameter-Vial
+    :greater-temperature-Coffee-temperature-Icecube]
+   {:nogood   #{[:greater-pressure-Beaker-pressure-Vial
+                 :greater-temperature-Coffee-temperature-Icecube]}
+    :children #{[:diameter-Beaker :temperature-Coffee]
+                [:diameter-Vial :temperature-Icecube]}}
+   [:flow-Beaker-Vial-Water-Pipe :flow-Coffee-Icecube-Heat-Bar]
+   {:children #{[:Beaker :Coffee]
+                [:Vial :Icecube]
+                [:Pipe :Bar]
+                [:Water :Heat]}}
+   [:greater-pressure-Beaker-pressure-Vial :greater-temperature-Coffee-temperature-Icecube]
+   {:nogood   #{[:greater-diameter-Beaker-diameter-Vial
+                 :greater-temperature-Coffee-temperature-Icecube]}
+    :children #{[:pressure-Vial :temperature-Icecube]
+                [:pressure-Beaker :temperature-Coffee]}}})
 
 
 (def expected-propagated-from-emaps
@@ -198,10 +199,7 @@
                                                                              [:Pipe :Bar]
                                                                              [:Vial :Icecube]}
                                                                    :nogood #{[:Water :Coffee]}}}
-    {[:greater-pressure-Beaker-pressure-Vial :greater-temperature-Coffee-temperature-Icecube]
-     {:emaps nil}
-     [:pressure-Beaker :temperature-Coffee]                       {:emaps nil}
-     [:flow-Beaker-Vial-Water-Pipe :flow-Coffee-Icecube-Heat-Bar] {:emaps nil}}))
+    nil))
 
 
 (def diameter-gmap {:mhs       #{[:greater-diameter-Beaker-diameter-Vial
