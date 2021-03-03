39c0c52193353105bdd6d3776bec13e7eb06e3e9
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-03 12:58:27 -0700

    Solar system analogy seems to be working as expected.

--- notebooks/solar_atom.clj
+++ notebooks/solar_atom.clj
@@ -6,6 +6,30 @@
             [sme-clj.typedef :as types]
             [ubergraph.core :as uber]))
 
+(defn map-vals
+  [f m]
+  (->> m
+    (map (juxt key (comp f val)))
+    (into {})))
+
+(defn as-graph
+  [m & {:keys [include-abstrs?]}]
+  (reduce
+    (fn [g [source {:keys [concept-graph] :as mop}]]
+      (reduce
+        (fn [g [role targets]]
+          (reduce
+            (fn [g target]
+              (uber/add-directed-edges g [source target {:label role}]))
+            g targets))
+        (uber/add-nodes-with-attrs g [source {:color (cond
+                                                       (#{#{:solar-system}} concept-graph)    :blue
+                                                       (#{#{:rutherford-atom}} concept-graph) :red
+                                                       :else                                  :green)}])
+        (dissoc mop :id :inst? :names :concept-graph (when-not include-abstrs? :parents))))
+    (uber/multidigraph)
+    (:mops m)))
+
 (def kg
   (as-> (mr/make-mop-map) m
     (types/initialize-kg m)
@@ -18,9 +42,9 @@
          [:revolve-around ::types/Relation nil ::types/Entity ::types/Entity]
          [:temperature ::types/Function nil ::types/Entity]
          [:cause ::types/Relation nil ::types/Expression ::types/Expression]
-         [:and ::types/Relation nil ::types/Expression ::types/Expression]
-         [:gravity ::types/Relation nil ::types/Expression ::types/Expression]
-         [:opposite-sign ::types/Relation nil ::types/Expression ::types/Expression]
+         [:and ::types/Relation {:ordered? false} ::types/Expression ::types/Expression]
+         [:gravity ::types/Function nil ::types/Expression ::types/Expression]
+         [:opposite-sign ::types/Function nil ::types/Expression ::types/Expression]
 
          [:Sun ::types/Entity nil]
          [:Planet ::types/Entity nil]
@@ -53,52 +77,30 @@
           [:attracts :Nucleus :Electron]]]))
     (mops/infer-hierarchy m)))
 
-(defn as-graph
-  [m & {:keys [include-abstrs?]}]
-  (reduce
-    (fn [g [source {:keys [concept-graph] :as mop}]]
-      (reduce
-        (fn [g [role targets]]
-          (reduce
-            (fn [g target]
-              (uber/add-directed-edges g [source target {:label role}]))
-            g targets))
-        (uber/add-nodes-with-attrs g [source {:color (if (#{#{:solar-system}} concept-graph)
-                                                       :blue
-                                                       :red)}])
-        (dissoc mop :id :inst? :names :concept-graph (when-not include-abstrs? :parents))))
-    (uber/multidigraph)
-    (:mops m)))
+;; Visualize the kg
+#_(let [boring-mops (-> (mr/make-mop-map)
+                      types/initialize-kg
+                      :mops
+                      keys
+                      set)]
+    (-> kg
+      (update :mops (fn [mops]
+                      (map-vals
+                        (fn [mop]
+                          (->> (dissoc mop :id :inst? :names)
+                            (remove (fn [[_ filler]]
+                                      (some boring-mops (cond-> filler (not (coll? filler)) vector))))
+                            (into {})))
+                        (apply dissoc mops (into boring-mops [:solar-system :rutherford-atom])))))
+      as-graph
+      uber/viz-graph))
 
-(defn map-vals
-  [f m]
-  (->> m
-    (map (juxt key (comp f val)))
-    (into {})))
-
-
-(let [boring-mops (-> (mr/make-mop-map)
-                    types/initialize-kg
-                    :mops
-                    keys
-                    set)]
-  (-> kg
-    (update :mops (fn [mops]
-                    (map-vals
-                      (fn [mop]
-                        (->> (dissoc mop :id :inst? :names)
-                          (remove (fn [[_ filler]]
-                                    (some boring-mops (cond-> filler (complement coll?) vector))))
-                          (into {})))
-                      (apply dissoc mops boring-mops))))
-    as-graph
-    uber/viz-graph))
-
-(->> kg
-  :mops
-  vals
-  (map :concept-graph))
-
-(->> (sme/match kg rules/analogy :solar-system :rutherford-atom)
+(->> rules/analogy
+  (sme/match kg :solar-system :rutherford-atom)
   (sme/perform-inference kg :solar-system)
+  (sort-by :score)
+  (reverse)
   (map :transferred))
+
+#_(rules/apply-rule kg (rules/analogy :compatible-args) [:cause-gravity-mass-Sun-mass-Planet-attracts-Sun-Planet
+                                                         :cause-opposite-sign-charge-Nucleus-charge-Electron-attracts-Nucleus-Electron])
