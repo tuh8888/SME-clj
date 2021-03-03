f3e8ffa2b49d0a1f2711cbadf174f382f6fe70b4
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-03 13:39:32 -0700

    Improved water-flow example.

--- notebooks/water_heat.clj
+++ notebooks/water_heat.clj
@@ -2,45 +2,47 @@
   (:require [mop-records :as mr]
             [mops :as mops]
             [sme-clj.core :as sme]
-            [sme-clj.typedef :as types]))
+            [sme-clj.typedef :as types]
+            [sme-clj.ruledef :as rules]))
 
 (def kg (-> (reduce (partial apply types/add-mop-entity)
-                         (types/initialize-kg (mr/make-mop-map))
-                         [[:cause ::types/Relation nil ::types/Expression ::types/Expression]
-                          [:greater ::types/Relation nil ::types/Expression ::types/Expression]
-                          [:flow ::types/Relation nil ::types/Entity ::types/Entity ::types/Entity ::types/Entity]
-                          [:pressure ::types/Function nil ::types/Entity]
-                          [:diameter ::types/Function nil ::types/Entity]
-                          [:clear ::types/Attribute nil ::types/Entity]
-                          [:temperature ::types/Function nil ::types/Entity]
-                          [:flat-top ::types/Function nil ::types/Entity]
-                          [:liquid ::types/Attribute nil ::types/Entity]
+              (types/initialize-kg (mr/make-mop-map))
+              [[:cause ::types/Relation nil ::types/Expression ::types/Expression]
+               [:greater ::types/Relation nil ::types/Expression ::types/Expression]
+               [:flow ::types/Relation nil ::types/Entity ::types/Entity ::types/Entity ::types/Entity]
+               [:pressure ::types/Function nil ::types/Entity]
+               [:diameter ::types/Function nil ::types/Entity]
+               [:clear ::types/Attribute nil ::types/Entity]
+               [:temperature ::types/Function nil ::types/Entity]
+               [:flat-top ::types/Function nil ::types/Entity]
+               [:liquid ::types/Attribute nil ::types/Entity]
 
-                          [:Coffee ::types/Entity nil]
-                          [:Water ::types/Entity nil]
-                          [:Heat ::types/Entity nil]
-                          [:Pipe ::types/Entity nil]
-                          [:Vial ::types/Entity nil]
-                          [:Icecube ::types/Entity nil]
-                          [:Bar ::types/Entity nil]
-                          [:Beaker ::types/Entity nil]])
-                     (types/add-concept-graph :simple-water-flow
-                       [:cause
-                        [:greater [:pressure :Beaker] [:pressure :Vial]]
-                        [:flow :Beaker :Vial :Water :Pipe]]
-                       [:greater [:diameter :Beaker] [:diameter :Vial]]
-                       [:clear :Beaker]
-                       [:flat-top :Water]
-                       [:liquid :Water])
-                     (types/add-concept-graph :simple-heat-flow
-                       [:flow :Coffee :Icecube :Heat :Bar]
-                       [:greater [:temperature :Coffee] [:temperature :Icecube]]
-                       [:flat-top :Coffee]
-                       [:liquid :Coffee])
-                     mops/infer-hierarchy))
+               [:Coffee ::types/Entity nil]
+               [:Water ::types/Entity nil]
+               [:Heat ::types/Entity nil]
+               [:Pipe ::types/Entity nil]
+               [:Vial ::types/Entity nil]
+               [:Icecube ::types/Entity nil]
+               [:Bar ::types/Entity nil]
+               [:Beaker ::types/Entity nil]])
+          (types/add-concept-graph :simple-water-flow
+            [:cause
+             [:greater [:pressure :Beaker] [:pressure :Vial]]
+             [:flow :Beaker :Vial :Water :Pipe]]
+            [:greater [:diameter :Beaker] [:diameter :Vial]]
+            [:clear :Beaker]
+            [:flat-top :Water]
+            [:liquid :Water])
+          (types/add-concept-graph :simple-heat-flow
+            [:flow :Coffee :Icecube :Heat :Bar]
+            [:greater [:temperature :Coffee] [:temperature :Icecube]]
+            [:flat-top :Coffee]
+            [:liquid :Coffee])
+          mops/infer-hierarchy))
 
-(sme/match kg :simple-water-flow :simple-heat-flow)
-
-(->> (sme/match kg :simple-water-flow :simple-heat-flow)
+(->> rules/analogy
+  (sme/match kg :simple-water-flow :simple-heat-flow)
   (sme/perform-inference kg :simple-water-flow)
-  (map :transferred))
+  (sort-by :score)
+  (reverse)
+  (map (juxt :score :transferred)))
