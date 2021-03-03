01afeb61eb7c80369463882452b7ae1aab98c039
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-03 03:08:38 -0700

    Working on visualizing these cleanly.

--- notebooks/solar_atom.clj
+++ notebooks/solar_atom.clj
@@ -2,8 +2,10 @@
   (:require [mop-records :as mr]
             [mops :as mops]
             [sme-clj.core :as sme]
+            [sme-clj.ruledef :as rules]
             [sme-clj.typedef :as types]
-            [sme-clj.ruledef :as rules]))
+            [ubergraph.core :as uber]))
+
 (def kg
   (as-> (mr/make-mop-map) m
     (types/initialize-kg m)
@@ -51,6 +53,52 @@
           [:attracts :Nucleus :Electron]]]))
     (mops/infer-hierarchy m)))
 
-(->> (sme/match kg rules/literal-similarity :solar-system :rutherford-atom)
+(defn as-graph
+  [m & {:keys [include-abstrs?]}]
+  (reduce
+    (fn [g [source {:keys [concept-graph] :as mop}]]
+      (reduce
+        (fn [g [role targets]]
+          (reduce
+            (fn [g target]
+              (uber/add-directed-edges g [source target {:label role}]))
+            g targets))
+        (uber/add-nodes-with-attrs g [source {:color (if (#{#{:solar-system}} concept-graph)
+                                                       :blue
+                                                       :red)}])
+        (dissoc mop :id :inst? :names :concept-graph (when-not include-abstrs? :parents))))
+    (uber/multidigraph)
+    (:mops m)))
+
+(defn map-vals
+  [f m]
+  (->> m
+    (map (juxt key (comp f val)))
+    (into {})))
+
+
+(let [boring-mops (-> (mr/make-mop-map)
+                    types/initialize-kg
+                    :mops
+                    keys
+                    set)]
+  (-> kg
+    (update :mops (fn [mops]
+                    (map-vals
+                      (fn [mop]
+                        (->> (dissoc mop :id :inst? :names)
+                          (remove (fn [[_ filler]]
+                                    (some boring-mops (cond-> filler (complement coll?) vector))))
+                          (into {})))
+                      (apply dissoc mops boring-mops))))
+    as-graph
+    uber/viz-graph))
+
+(->> kg
+  :mops
+  vals
+  (map :concept-graph))
+
+(->> (sme/match kg rules/analogy :solar-system :rutherford-atom)
   (sme/perform-inference kg :solar-system)
   (map :transferred))
